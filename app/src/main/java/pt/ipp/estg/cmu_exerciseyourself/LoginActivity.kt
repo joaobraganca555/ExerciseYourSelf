9a6a719f8116a6c3bca2fa0b2dd8f563e5569238
JOÃO MIGUEL TEIXEIRA BRAGANÇA <8190555@estg.ipp.pt>
2022-01-28 02:39:52 +0000

    login update

--- app/src/main/java/pt/ipp/estg/cmu_exerciseyourself/LoginActivity.kt
+++ app/src/main/java/pt/ipp/estg/cmu_exerciseyourself/LoginActivity.kt
@@ -1,11 +1,20 @@
 package pt.ipp.estg.cmu_exerciseyourself
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlin.math.log
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        var loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
