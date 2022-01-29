a4db22b305834eb6af94749596368e9163052f49
JOÃO MIGUEL TEIXEIRA BRAGANÇA <8190555@estg.ipp.pt>
2022-01-29 17:45:35 +0000

    update register

--- app/src/main/java/pt/ipp/estg/cmu_exerciseyourself/ui/authentication/RegisterFragment.kt
+++ app/src/main/java/pt/ipp/estg/cmu_exerciseyourself/ui/authentication/RegisterFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.textfield.TextInputLayout
 import pt.ipp.estg.cmu_exerciseyourself.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -16,6 +21,10 @@ import pt.ipp.estg.cmu_exerciseyourself.R
  */
 class RegisterFragment : Fragment() {
     // TODO: Rename and change types of parameters
+    lateinit var registerButton : Button
+    lateinit var birthDateLayout : TextInputLayout
+    lateinit var birthDateText : TextView
+    lateinit var birthDateImage : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +37,24 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+
+        registerButton = view.findViewById(R.id.registerButton)
+        birthDateLayout = view.findViewById(R.id.birthDateLayout)
+        birthDateText = view.findViewById(R.id.birthDateText)
+        birthDateImage = view.findViewById(R.id.birthDateImageButton)
+
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Selecione a data")
+                .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                .build()
+
+        birthDateImage.setOnClickListener {
+            datePicker.show(parentFragmentManager,datePicker.toString())
+        }
+
+        return view
     }
 
 }
