16df8deac4631499675af8671dfbb7793f6ef0c5
JOÃO MIGUEL TEIXEIRA BRAGANÇA <8190555@estg.ipp.pt>
2022-01-31 04:45:18 +0000

    discover update

---
+++ app/src/main/java/pt/ipp/estg/cmu_exerciseyourself/model/retrofit/GeopifyResponse.kt
@@ -0,0 +1,19 @@
+package pt.ipp.estg.cmu_exerciseyourself.model.retrofit
+
+import com.google.gson.annotations.SerializedName
+
+
+data class ResponseObject (
+    @SerializedName("features" ) var features : ArrayList<Features> = arrayListOf()
+)
+
+data class Features (
+    @SerializedName("properties" ) var properties : Properties? = Properties(),
+)
+
+data class Properties (
+    @SerializedName("lon"           ) var lon          : Double?           = null,
+    @SerializedName("lat"           ) var lat          : Double?           = null,
+    @SerializedName("formatted"     ) var formatted    : String?           = null,
+    @SerializedName("categories"    ) var categories   : ArrayList<String> = arrayListOf()
+)
--- app/src/main/java/pt/ipp/estg/cmu_exerciseyourself/ui/discover/DiscoverFragment.kt
+++ app/src/main/java/pt/ipp/estg/cmu_exerciseyourself/ui/discover/DiscoverFragment.kt
@@ -1,16 +1,48 @@
 package pt.ipp.estg.cmu_exerciseyourself.ui.discover
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.textfield.TextInputEditText
 import pt.ipp.estg.cmu_exerciseyourself.R
+import pt.ipp.estg.cmu_exerciseyourself.model.retrofit.ResponseObject
+import pt.ipp.estg.trashtalkerapp.retrofitService.TrashtalkerAPI
+import retrofit2.Call
+import retrofit2.Response
+import java.util.concurrent.Executors
 
 class DiscoverFragment : Fragment() {
+    private lateinit var myContext : Context
+    private lateinit var findMySelfButton : Button
+    private lateinit var findButton : Button
+    private lateinit var mapFragment : SupportMapFragment
+    private lateinit var radiusText : TextInputEditText
+    private lateinit var geopifyResponseObject: ResponseObject
+    private val felgueiras = LatLng(41.36735, -8.20094)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    private val callback = OnMapReadyCallback { googleMap ->
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(felgueiras)
+                .title("Eu")
+        )
+        googleMap.animateCamera( CameraUpdateFactory.newLatLngZoom(felgueiras,10f))
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        myContext = context
     }
 
     override fun onCreateView(
@@ -18,6 +50,44 @@ class DiscoverFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_discover, container, false)
+        var view = inflater.inflate(R.layout.fragment_discover, container, false)
+
+        findMySelfButton = view.findViewById(R.id.findMySelfButton)
+        findButton = view.findViewById(R.id.findButton)
+        radiusText = view.findViewById(R.id.radiusText)
+
+        findMySelfButton.setOnClickListener {
+            mapFragment?.getMapAsync(callback)
+        }
+
+
+        findButton.setOnClickListener {
+            Log.d("asd",radiusText.text.toString())
+                var retrofitClient = TrashtalkerAPI.getApi()
+                val responseCallback = retrofitClient.findPlaces(radiusText.text.toString())
+                responseCallback.enqueue(object: retrofit2.Callback<ResponseObject> {
+                    override fun onResponse(call: Call<ResponseObject>, response: Response<ResponseObject>) {
+                        if(response.code() === 200){
+                            Log.d("asd", response.body().toString())
+                            geopifyResponseObject = response.body() as ResponseObject
+                        } else{
+                            Toast.makeText(myContext,"Something went wrong!", Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ResponseObject>, t: Throwable) {
+                        Toast.makeText(myContext,t.message.toString(), Toast.LENGTH_LONG).show()
+                    }
+                })
+        }
+
+        return view
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapFragment = (childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?)!!
+        mapFragment?.getMapAsync(callback)
+    }
+
 }
